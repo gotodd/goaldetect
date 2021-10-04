c439981abbd267c9d93eda48c7a8c6acf5730446
Todd <toddzakk@gmail.com>
2021-10-03 17:28:24 -0700

    removed files from git .cxx; fixed visionupdate json message

--- face-detection/src/com/team254/cheezdroid/comm/messages/TargetUpdateMessage.java
+++ face-detection/src/com/team254/cheezdroid/comm/messages/TargetUpdateMessage.java
@@ -20,4 +20,19 @@ public class TargetUpdateMessage extends VisionMessage {
     public String getMessage() {
         return mUpdate.getSendableJsonString(mTimestamp);
     }
+
+    @Override
+    public String toJson() {
+      return "{\"type\":\""+getType()+"\",\"message\":"+getMessage()+"}";
+        /*
+      JSONObject j = new JSONObject();
+      try {
+        j.put("type", getType());
+        j.put("message", getMessage());
+      } catch (JSONException e) {
+        Log.e("VisionMessage", "Could not encode JSON");
+      }
+      return j.toString();
+      */
+    }
 }
